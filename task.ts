fe8d5087afe712ce5d2954b2fbbe2d2c9c3bf6a0
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2018-03-02 20:26:49 +0100

    Added a test with strings for an assertion of equality of array items.

--- task.ts
+++ task.ts
@@ -1,30 +1,34 @@
 let expected = true;
 let actual = arrayItemsEqual([true, false], [true, false]);
-assert('Array contains items: should accept booleans', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should accept booleans', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+
+expected = true;
+actual = arrayItemsEqual(['a', 'b'], ['a', 'b']);
+assert('arrayItemsEqual: should accept strings', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = true;
 actual = arrayItemsEqual([1, 2, 3], [1, 2, 3]);
-assert('Array contains items: should accept numbers', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should accept numbers', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = false;
 actual = arrayItemsEqual([1, 2, 3], [1, 2, 4]);
-assert('Array contains items: should not accept different numbers', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should not accept different numbers', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = false;
 actual = arrayItemsEqual([1, 2, 3], [1, 3, 2]);
-assert('Array contains items: should not accept different order', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should not accept different order', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = false;
 actual = arrayItemsEqual([1, 2, 3], []);
-assert('Array contains items: should not accept different cardinality', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should not accept different cardinality', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = false;
 actual = arrayItemsEqual(null, [1, 2]);
-assert('Array contains items: should not accept that first is null', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should not accept that first is null', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 expected = false;
 actual = arrayItemsEqual([1, 2], null);
-assert('Array contains items: should not accept that second is null', expected === actual, `Expected ${expected}, but was: ${actual}.`);
+assert('arrayItemsEqual: should not accept that second is null', expected === actual, `Expected ${expected}, but was: ${actual}.`);
 
 function arrayItemsEqual<T>(array: T[], expectedItems: T[]) {
   if (!array || !expectedItems)
