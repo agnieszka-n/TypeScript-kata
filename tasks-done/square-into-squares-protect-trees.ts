bfdbcc1c923dd80d8e2d1fdbf21ba022fbca27aa
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2018-04-11 21:56:32 +0200

    Added a solution for square-into-squares-protect-trees.

---
+++ tasks-done/square-into-squares-protect-trees.ts
@@ -0,0 +1,72 @@
+export class G964 {
+
+  public static decompose = (n) => {
+    let sequence: number[] = [];
+    let squaresSumToDecompose = n * n;
+    let startNumber = n - 1;
+
+    do {
+      let nextNumberInSequence = Math.floor(Math.sqrt(squaresSumToDecompose));
+
+      if (nextNumberInSequence > startNumber)
+        nextNumberInSequence = startNumber;
+
+      if (nextNumberInSequence === 0) {
+        let lastNumberInSequence: number;
+
+        do {
+          lastNumberInSequence = sequence.pop();
+          squaresSumToDecompose += Math.pow(lastNumberInSequence, 2);
+        }
+        while (lastNumberInSequence === 1 && sequence.length > 0);
+
+        if (sequence.length === 0)
+          return null;
+
+        sequence.push(lastNumberInSequence - 1);
+        squaresSumToDecompose -= Math.pow(lastNumberInSequence - 1, 2);
+        startNumber = lastNumberInSequence - 2;
+        continue;
+      }
+
+      const nextNumberSquare = Math.pow(nextNumberInSequence, 2);
+      if (squaresSumToDecompose >= nextNumberSquare) {
+        sequence.push(nextNumberInSequence);
+        squaresSumToDecompose -= nextNumberSquare;
+      }
+      startNumber = nextNumberInSequence - 1;
+    }
+    while (squaresSumToDecompose !== 0);
+
+    return sequence.reverse();
+  }
+}
+
+/*
+
+const testCases: TestCase[] = [
+  {
+    params: [11],
+    expectedResult: [1, 2, 4, 10]
+  },
+  {
+    params: [50],
+    expectedResult: [1, 3, 5, 8, 49]
+  },
+  {
+    params: [44],
+    expectedResult: [2, 3, 5, 7, 43]
+  },
+  {
+    params: [7],
+    expectedResult: [2, 3, 6]
+  },
+  {
+    params: [4],
+    expectedResult: null
+  }
+];
+
+executeTestCases(testCases, G964.decompose, (actual, expected) => arrayItemsEqual(actual, expected), 'G964.decompose');
+
+*/
