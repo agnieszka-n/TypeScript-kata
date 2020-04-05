3b2adb9c3d56c07fee5a0d9b1e50d524221c955f
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2020-04-05 21:50:41 +0200

    Made test assertions static.

--- areArrayItemsEqual.tests.ts
+++ areArrayItemsEqual.tests.ts
@@ -1,27 +1,27 @@
-import { areArrayItemsEqual, assertTrue, assertFalse } from './testing-environment';
+import { Assert } from './testing-environment';
 
 export function executeTests() {
-  let result = areArrayItemsEqual([true, false], [true, false]);
-  assertTrue('areArrayItemsEqual: should accept booleans', result);
+  let result = Assert.areArrayItemsEqual([true, false], [true, false]);
+  Assert.assertTrue('areArrayItemsEqual: should accept booleans', result);
 
-  result = areArrayItemsEqual(['a', 'b'], ['a', 'b']);
-  assertTrue('areArrayItemsEqual: should accept strings', result);
+  result = Assert.areArrayItemsEqual(['a', 'b'], ['a', 'b']);
+  Assert.assertTrue('areArrayItemsEqual: should accept strings', result);
 
-  result = areArrayItemsEqual([1, 2, 3], [1, 2, 3]);
-  assertTrue('areArrayItemsEqual: should accept numbers', result);
+  result = Assert.areArrayItemsEqual([1, 2, 3], [1, 2, 3]);
+  Assert.assertTrue('areArrayItemsEqual: should accept numbers', result);
 
-  result = areArrayItemsEqual([1, 2, 3], [1, 2, 4]);
-  assertFalse('areArrayItemsEqual: should not accept different numbers', result);
+  result = Assert.areArrayItemsEqual([1, 2, 3], [1, 2, 4]);
+  Assert.assertFalse('areArrayItemsEqual: should not accept different numbers', result);
 
-  result = areArrayItemsEqual([1, 2, 3], [1, 3, 2]);
-  assertFalse('areArrayItemsEqual: should not accept different order', result);
+  result = Assert.areArrayItemsEqual([1, 2, 3], [1, 3, 2]);
+  Assert.assertFalse('areArrayItemsEqual: should not accept different order', result);
 
-  result = areArrayItemsEqual([1, 2, 3], []);
-  assertFalse('areArrayItemsEqual: should not accept different cardinality', result);
+  result = Assert.areArrayItemsEqual([1, 2, 3], []);
+  Assert.assertFalse('areArrayItemsEqual: should not accept different cardinality', result);
 
-  result = areArrayItemsEqual(null, [1, 2]);
-  assertFalse('areArrayItemsEqual: should not accept that first is null', result);
+  result = Assert.areArrayItemsEqual(null, [1, 2]);
+  Assert.assertFalse('areArrayItemsEqual: should not accept that first is null', result);
 
-  result = areArrayItemsEqual([1, 2], null);
-  assertFalse('areArrayItemsEqual: should not accept that second is null', result);
+  result = Assert.areArrayItemsEqual([1, 2], null);
+  Assert.assertFalse('areArrayItemsEqual: should not accept that second is null', result);
 }
--- testing-environment.ts
+++ testing-environment.ts
@@ -3,68 +3,70 @@ interface TestCase {
   expectedResult: any;
 }
 
-function executeTestCases(testCases: TestCase[], functionUnderTest: Function, testCallback: (actualResult: any, expectedResult: any) => boolean, titlePrefix?: string) {
+export function executeTestCases(testCases: TestCase[], functionUnderTest: Function, testCallback: (actualResult: any, expectedResult: any) => boolean, titlePrefix?: string) {
   for (let i = 0; i < testCases.length; i++) {
     const testCase = testCases[i];
     const actualResult = functionUnderTest.apply(undefined, testCase.params);
     const prefix = titlePrefix ? titlePrefix : `${functionUnderTest.toString()}`;
     const title = `${prefix} (${i + 1}), params: ${testCase.params}`;
-    assertTrue(title, testCallback(actualResult, testCase.expectedResult), `${title} - Expected: ${testCase.expectedResult}, but was: ${actualResult}.`);
+    Assert.assertTrue(title, testCallback(actualResult, testCase.expectedResult), `${title} - Expected: ${testCase.expectedResult}, but was: ${actualResult}.`);
   }
 }
 
-export function areArrayItemsEqual(array: any[], expectedItems: any[]): boolean {
-  if (!array && !expectedItems)
-    return true;
+export class Assert {
+  static areArrayItemsEqual(array: any[], expectedItems: any[]): boolean {
+    if (!array && !expectedItems)
+      return true;
 
-  if (!array || !expectedItems)
-    return false;
-
-  if (array.length !== expectedItems.length)
-    return false;
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] instanceof Array) {
-      if (!areArrayItemsEqual(array[i], expectedItems[i])) {
-        return false;
-      }
-    }
-    else if (array[i] !== expectedItems[i])
+    if (!array || !expectedItems)
       return false;
+
+    if (array.length !== expectedItems.length)
+      return false;
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] instanceof Array) {
+        if (!Assert.areArrayItemsEqual(array[i], expectedItems[i])) {
+          return false;
+        }
+      }
+      else if (array[i] !== expectedItems[i])
+        return false;
+    }
+
+    return true;
   }
 
-  return true;
-}
+  static assertTrue(title: string, testExpression: boolean, failMessage?: string) {
+    const testsRoot = document.getElementById('testResults');
+    const currentTestElement = document.createElement('li');
+    currentTestElement.classList.add('test');
 
-export function assertTrue(title: string, testExpression: boolean, failMessage?: string) {
-  const testsRoot = document.getElementById('testResults');
-  const currentTestElement = document.createElement('li');
-  currentTestElement.classList.add('test');
+    testsRoot.appendChild(currentTestElement);
 
-  testsRoot.appendChild(currentTestElement);
-
-  if (testExpression) {
-    currentTestElement.innerText = title;
-    currentTestElement.classList.add('test-passed');
+    if (testExpression) {
+      currentTestElement.innerText = title;
+      currentTestElement.classList.add('test-passed');
+    }
+    else {
+      currentTestElement.innerText = failMessage ? failMessage : title;
+      currentTestElement.classList.add('test-failed');
+    }
   }
-  else {
-    currentTestElement.innerText = failMessage ? failMessage : title;
-    currentTestElement.classList.add('test-failed');
+
+  static assertFalse(title: string, testExpression: boolean, failMessage?: string) {
+    Assert.assertTrue(title, !testExpression, failMessage);
   }
-}
 
-export function assertFalse(title: string, testExpression: boolean, failMessage?: string) {
-  assertTrue(title, !testExpression, failMessage);
-}
+  static assertEqual(title: string, first: any, second: any) {
+    const actualTitle = title ? title : `${first} === ${second}`;
 
-function assertEqual(title: string, first: any, second: any) {
-  const actualTitle = title ? title : `${first} === ${second}`;
+    Assert.assertTrue(actualTitle, first === second, `${actualTitle} - Expected ${first} to equal ${second}.`);
+  }
 
-  assertTrue(actualTitle, first === second, `${actualTitle} - Expected ${first} to equal ${second}.`);
-}
+  static assertArrayItemsEqual(title: string, first: any[], second: any[]) {
+    const actualTitle = title ? title : `${first} === ${second}`;
 
-function assertArrayItemsEqual(title: string, first: any[], second: any[]) {
-  const actualTitle = title ? title : `${first} === ${second}`;
-
-  assertTrue(actualTitle, areArrayItemsEqual(first, second), `${actualTitle} - Expected [${first}] to equal [${second}].`);
+    Assert.assertTrue(actualTitle, Assert.areArrayItemsEqual(first, second), `${actualTitle} - Expected [${first}] to equal [${second}].`);
+  }
 }
